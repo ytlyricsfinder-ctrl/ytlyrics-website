1f5433ff3a61b7367902716c2ba24f72de38d760
ytlyricsfinder-ctrl <ytlyricsfinder@gmail.com>
2026-01-19 18:21:38 +0800

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import AdPlaceholder from "@/components/AdPlaceholder";
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
-{/* Navigation */}
+      {/* Navigation */}
       <nav className="border-b border-gray-200">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
